be8458e13f7a6a4ca57759e83bd210ef19be331e
bimaandhika <bimaaandhika304@gmail.com>
2025-10-22 02:42:52 +0800

    postest6

--- P1.py
+++ P1.py
@@ -1,1 +1,6 @@
-print("Hello world")
+# list mahasiswa
+mahasiswa = [["Daffa", "Dante", "Santoso"], ["Pernanda", "Triya", "Ahnaf"]]
+# perulangan for untuk mendapatkan semua elemen
+ for i in mahasiswa:
+    for j in i :
+        print (j)
